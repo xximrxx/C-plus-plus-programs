cb3cc4053c23802f3bfe7589df76aeeff75211a4
xximrxx <88969962+xximrxx@users.noreply.github.com>
2021-08-24 23:59:52 +0530

    Update Palindrome String.cpp

--- String/Palindrome String.cpp
+++ String/Palindrome String.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char str;
+    cout<<"Input a string -> ";
+    cin>>str;
+    int l=0;
+    int h=strlen(str)-1;
+    while(h>l)
+    {
+        if(str[l++]!=str[h--])
+        cout<<str<<" is not a palindrome.";
+        else
+        cout<<str<<" is a palindrome. "
+    }
+    return 0;
+}
+
+/*
+Output
+Input a string -> abababa
+abababa is a palindrome
+*/
 

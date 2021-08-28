e4a775fc27e4a12df7582c69d65ee5ed93618cb9
xximrxx <88969962+xximrxx@users.noreply.github.com>
2021-08-28 21:29:24 +0530

    Update Insertion in Linked List.cpp

--- Linked List/Insertion in Linked List.cpp
+++ Linked List/Insertion in Linked List.cpp
@@ -31,8 +31,9 @@ void append()
 
 }
 
-void Display(struct Node* ptr)
+void Display()
 {
+   struct Node* ptr
   cout<<"Linked List created is ";
     while (ptr!=NULL)
     {
@@ -60,6 +61,30 @@ void insertatbegin() // creating positioin and the value to be inserted
 
 int main()
 {
-   
+   int choice;
+        while(1){
+               
+                
+                cout<<"\n Press 1 to create linkedt list i.e., insertion";
+                cout<<"\n Press 2 to display linked list elements Display";
+                cout<<"\n Press 3 to insert an element in linked list at begining";
+                cout<<"\n Enter your choice: ";
+                cin>>choice;
+                switch(choice)
+                {
+                        case 1:
+                                        append();
+                                        break;
+                        case 2:
+                                        display();
+                                        break;
+
+                        case 3:         insertatbegin();
+                                        break;
+                        default:
+                                    cout<<"Wrong choice";
+                                    exit(0);
+                }
+        }
     return 0;
 }

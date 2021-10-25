7a4dbfeabbcb2eb9cf07157a455161e9e19fe108
xximrxx <88969962+xximrxx@users.noreply.github.com>
2021-10-25 15:39:55 +0530

    Create Longest Consecutive 1's.cpp

---
+++ Bit Masking/Longest Consecutive 1's.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int consecutive_ones(int num1)
+{
+  int count=0;
+  while(num1!=0)
+  {
+    num1=(num1&(num1<<1));
+    count++;
+  }
+  return count;
+}
+
+int main()
+{
+  int num1;
+  cin>>num1;
+  int ans;
+  ans=consecutive_ones(num1);
+  cout<<"Maximum consecutive ones are" <<ans;
+  return 0;
+}
+

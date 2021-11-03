784a2377ac7f324b094e1883384b1b24aae72ad8
xximrxx <88969962+xximrxx@users.noreply.github.com>
2021-11-03 19:10:31 +0530

    Update Wave Array.cpp

--- Sorting/Wave Array.cpp
+++ Sorting/Wave Array.cpp
@@ -4,24 +4,28 @@ using namespace std;
 
 void swap(int *x, int *y)
 {
-    int temp = *x;
-    *x = *y;
-    *y = temp;
+    int temp=*x;
+    *x=*y;
+    *y=temp;
 }
 
 void sortInWave(int arr[], int n)
 {
     sort(arr, arr+n);
-    for (int i=0; i<n-1; i += 2)
+    for(int i=0; i<n-1; i += 2)
         swap(&arr[i], &arr[i+1]);
 }
 
 int main()
 {
-    int arr[] = {10, 90, 49, 2, 1, 5, 23};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int n;
+    cout<<"Enter size of array-> ";
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
     sortInWave(arr, n);
     for (int i=0; i<n; i++)
-       cout << arr[i] << " ";
+       cout<<arr[i]<< " ";
     return 0;
 }

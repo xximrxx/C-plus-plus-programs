098e08d8d91b0495c9f32d62c320a4907108c496
xximrxx <88969962+xximrxx@users.noreply.github.com>
2021-11-05 21:28:48 +0530

    Create Maximum Index.cpp

---
+++ Sorting/Maximum Index.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int maxIndexDiff(int arr[], int n)
+{
+    int maxDiff = -1;
+    int i, j;
+
+    for (i = 0; i < n; ++i) 
+    {
+        for (j = n - 1; j > i; --j) 
+        {
+            if (arr[j] > arr[i] && maxDiff < (j - i))
+                maxDiff = j - i;
+        }
+    }
+
+    return maxDiff;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {
+      cin>>arr[n];
+    }
+    int maxDiff=maxIndexDiff(arr, n);
+    cout<<maxDiff;
+    return 0;
+}
